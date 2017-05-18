397f2f94cdf6c855ba030fd9e240d5716959bdbd
marinamaluta <marinaqueirozmaluta@gmail.com>
2017-05-18 19:30:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
         offset: 51
     });
 
-    // Função para fechar o menu responsivo e ao clicar o oitem
+    // Função para fechar o menu responsivo e ao clicar o item
     $('.navbar-collapse ul li a').click(function(){ 
             $('.navbar-toggle:visible').click();
     });
@@ -30,29 +30,41 @@
         }
     })
 
-})(jQuery); // Final do "use strict"
+})(jQuery);
+ // Final do "use strict"
 
  // Inicio do JS do formulário
 
 var fonte = document.querySelector('[data-contador-fonte]');
+var inputs = document.querySelectorAll('#contactForm input');
 var destino = document.querySelector('[data-contador-destino]');
+
+for (var i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    input.addEventListener('keydown', function() {
+        this.parentNode.classList.remove('nao-toquei');
+    })
+}
 
 fonte.addEventListener('keyup', function() {
   var digitado = fonte.value.length;
   var restante = 150 - digitado;
   destino.textContent = restante;
+  this.parentNode.classList.remove('nao-toquei');
 });
 
 var formulario = document.querySelector('form');
 
 formulario.addEventListener('submit', function(evento) {
-  alert('Obrigada pelo seu contato!');
   evento.preventDefault();
+  if(this.checkValidity() == true) {
+    alert('Obrigada pelo seu contato! Qualquer dúvida, entre em contato diretamente pelo tel (11) 9999-9999');
+  } else {
+    alert('Preencha todos os campos corretamente')
+  }
 });
 
 
 // FInal do JS do formulário
-
-
 
 // Final do JavaScript
